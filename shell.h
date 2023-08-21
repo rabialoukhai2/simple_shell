6c129b41742c80a6c982bf4c36e1ee986eb9f10b
rabialoukhai2 <134720091+rabialoukhai2@users.noreply.github.com>
2023-08-21 11:15:28 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,28 +1,27 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#define BUFFERSIZE 1024
-#define DELIMINATOR "\n\r\t\a "
 
-extern char **environ;
 
-#include <errno.h>
-#include <fcntl.h>
-#include <limits.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
 #include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+#define DELIMINATOR "\n\r\t\a "
+#define BUFFERSIZE 1024
+extern char **environ;
+
 
 /**
- * struct list_s - singly linked list
- * singly linked list node structr
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * struct list_s - singly linked list struct node
  */
 
 typedef struct list_s
