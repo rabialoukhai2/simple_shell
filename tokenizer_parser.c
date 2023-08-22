b4d7c19d0a10c2f7e8e8f1b12e13421a745c217d
rabialoukhai2 <rloukhi@gmail.com>
2023-08-22 04:20:16 -0700

    commit task

---
+++ tokenizer_parser.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+/**
+ * parse_ln - Put arguments in the arr
+ */
+
+char **parse_ln(char *line)
+{
+	int buffer_size = BUFFERSIZE;
+	int location = 0;
+	char **tokens = NULL;
+	char *token = NULL;
+
+	tokens = malloc(sizeof(char *) * buffer_size);
+	if (!tokens)
+	{
+		perror("hsh");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(line, DELIMINATOR);
+
+	while (token != NULL)
+	{
+		tokens[location] = token;
+		token = strtok(NULL, DELIMINATOR);
+		location++;
+	}
+	if (location >= buffer_size)
+	{
+		buffer_size += BUFFERSIZE;
+		tokens = _reallocat(tokens, BUFFERSIZE,buffer_size * sizeof(char *));
+
+		if (!tokens)
+		{
+			{
+				perror("hsh: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, DELIMINATOR);
+	}
+	tokens[location] = NULL;
+	return (tokens);
+}
