13cd3f9c0d61122a1c8b3a3b77af2c8d6af254ce
rabialoukhai2 <134720091+rabialoukhai2@users.noreply.github.com>
2023-08-23 23:56:07 +0100

    Update fn1.c

--- fn1.c
+++ fn1.c
@@ -3,34 +3,34 @@
 /**
  * fn1 - Checks if the input
  * @str: string
- * @env: environement
+ * @env: environment
  * Return: multiple 1
  */
 
 int fn1(char **str, char **env)
 {
 pid_t pid, wpid;
-int id = 0, i = 0;
-char *sep = "/";
+int i = 0;
 char *copy = NULL;
-pid = fork();
-if (pid == 0)
+copy = _strdup(str[0]);
+if ((access(str[0], R_OK | X_OK)) == 0)
 {
-id = execve(str[0], str, env);
-if (id == -1)
+int result = execute_and_check(str, env);
+free(copy);
+return (result);
+}
+else if (copy != NULL)
 {
+for (i = 0; copy[i] != '\0'; i++) {
+if (copy[i] == '/')
+{
+errno = ENOENT;
 perror("hsh");
-}
-exit(EXIT_SUCCESS);
-}
-else if (pid < 0)
-perror("hsh");
-else
-do {
-wpid = waitpid(pid, &id, WUNTRACED);
-if (wpid == -1)
-perror("hsh");
-} while (!WIFSIGNALED(id) && !WIFEXITED(id));
 free(copy);
 return (1);
+}
+}
+}
+free(copy);
+return (0);
 }
