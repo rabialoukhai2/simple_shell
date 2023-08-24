5df6b086408960f50995ce0e77b48481741c397b
MD-dev-web <123594674+MD-dev-web@users.noreply.github.com>
2023-08-24 23:02:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -16,8 +16,7 @@ int main(void)
 
 	while (1)
 	{
-		if (is_interactive)
-			if(tests(chars_read)==1) break;
+		if (is_interactive && tests(chars_read)==1) break;
 		else
 		{
 			chars_read = getline(&command, &bufsize, stdin);
